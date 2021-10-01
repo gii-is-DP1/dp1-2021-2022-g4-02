c3ddc6d4b5d17842e26cfa78d3d2f22b3b7921cb
Ivan <ivan.moreno.granado@gmail.com>
2021-10-01 18:31:27 +0200

    Personalización de la pantalla de inicio

--- src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
@@ -1,7 +1,10 @@
 package org.springframework.samples.petclinic.web;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.samples.petclinic.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,7 +14,36 @@ public class WelcomeController {
 	
 	  @GetMapping({"/","/welcome"})
 	  public String welcome(Map<String, Object> model) {	    
-
+		  
+		  /* First, we create a list of person type */
+		  List<Person> people = new ArrayList<Person>();
+		  /*Then, we create new person objects and add to the list */
+		 
+		  Person personone = new Person();
+		  personone.setFirstName("Iván ");
+		  personone.setLastName("Moreno Granado");
+		  personone.setId(1);
+		  people.add(personone);
+		  
+		  Person persontwo = new Person();
+		  persontwo.setFirstName("Dámaris ");
+		  persontwo.setLastName("Gómez Serrano");
+		  persontwo.setId(2);
+		  people.add(persontwo);
+		  
+		  
+		  Person personthree = new Person();
+		  personthree.setFirstName("Miguel Ángel ");
+		  personthree.setLastName("Rivas Rosado");
+		  personthree.setId(3);
+		  people.add(personthree);
+		  
+		  /*Finally, we put the list on model*/
+		  
+		  model.put("persons",people);
+		  model.put("title","Our project");
+		  model.put("group", "Developers");
+		
 	    return "welcome";
 	  }
 }
