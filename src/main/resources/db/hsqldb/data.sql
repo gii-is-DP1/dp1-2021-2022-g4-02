986ec525b493a41b0c745f8f9f3161800e63e583
Rafael Sanabriagit <rafsanesp@alum.us.es>
2021-10-04 13:25:27 +0200

    Añadido como dueño

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -22,6 +22,10 @@ INSERT INTO authorities(id,username,authority) VALUES (6,'ivamorgra','owner');
 INSERT INTO users(username,password,enabled) VALUES ('antlopcub','antlopcub',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (7,'antlopcub','owner');
 
+-- Usuario de Rafa como Owner (dueño)
+INSERT INTO users(username,password,enabled) VALUES ('rafsanesp','rafsanesp',TRUE);
+INSERT INTO authorities(id,username,authority) VALUES (8,'rafsanesp','owner');
+
 INSERT INTO vets VALUES (1, 'James', 'Carter');
 INSERT INTO vets VALUES (2, 'Helen', 'Leary');
 INSERT INTO vets VALUES (3, 'Linda', 'Douglas');
@@ -57,6 +61,7 @@ INSERT INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085
 INSERT INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683', 'owner1');
 INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435', 'owner1');
 INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487', 'owner1');
+
 -- Datos del dueño Miguel
 INSERT INTO owners VALUES (11, 'Miguel Ángel', 'Rivas Rosado', 'Calle José de Soto y Molina', 'Jerez de la Frontera', '636947113', 'migrivros');
 -- Datos del dueño Jose
@@ -65,6 +70,9 @@ INSERT INTO owners VALUES (12, 'José Manuel', 'Martín Luque', 'Calle Mendez Nu
 INSERT INTO owners VALUES (13,'Iván','Moreno Granado','Calle Belmonte','Guillena','608299562','ivamorgra');
 -- Datos del dueño Antonio
 INSERT INTO owners VALUES (14,'Antonio José','López Cubiles','Calle Virgen de Guia','Castilleja de la Cuesta','640605765','antlopcub');
+-- Datos del dueño Rafa
+INSERT INTO owners VALUES (15,'Rafael','Sanabria Espárrago','Calle Cardenal Ilundain','Sevilla','674907182','rafsanesp');
+
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (1, 'Leo', '2010-09-07', 1, 1);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (2, 'Basil', '2012-08-06', 6, 2);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (3, 'Rosy', '2011-04-17', 2, 3);
@@ -78,6 +86,7 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (10, 'Mulligan', '2
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (11, 'Freddy', '2010-03-09', 5, 9);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
+
 -- Mascota de Miguel
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Scooby', '2019-01-20', 2, 11);
 -- Mascota de Jose
@@ -88,6 +97,9 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (17,'Budy','2019-01
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (18,'Coco','2021-09-25',5,13);
 -- Mascota de Antonio
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (19,'Limón','2021-07-7',1,14);
+-- Mascota de Rafa
+INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (20,'Aguacate','2014-01-7',4,15);
+
 
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02', 'rabies shot');
