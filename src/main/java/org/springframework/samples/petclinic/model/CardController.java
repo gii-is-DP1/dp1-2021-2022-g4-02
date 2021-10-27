0beb92cc43bdb9bcd2afe061fe5f95a5aaa5cd33
Miguel Ángel Rivas Rosado <migrivros@alum.us.es>
2021-10-27 22:00:14 +0200

    Realizando la vista de edición de cartas (aún no funciona)

--- src/main/java/org/springframework/samples/petclinic/model/CardController.java
+++ src/main/java/org/springframework/samples/petclinic/model/CardController.java
@@ -1,14 +1,23 @@
 package org.springframework.samples.petclinic.model;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/cards")
 public class CardController {
+	
+	private static final String VIEWS_CARDS_CREATE_OR_UPDATE_FORM = "cards/createOrUpdateCardForm";
 	
 	@Autowired
 	private CardService cardService;
@@ -19,5 +28,40 @@ public class CardController {
 		Iterable<Card> cards = cardService.cardFindAll();
 		modelMap.addAttribute("cards", cards);
 		return vista;
+	}
+	
+	
+	@GetMapping(value = "/cards/{cardId}/edit")
+	public String initUpdateForm(@PathVariable("cardId") int cardId, ModelMap model) {
+		Card card = this.cardService.findCardById(cardId);
+		model.put("card", card);
+		return VIEWS_CARDS_CREATE_OR_UPDATE_FORM;
+	}
+
+    /**
+     *
+     * @param card
+     * @param result
+     * @param cardId
+     * @param model
+     * @return
+     */
+        @PostMapping(value = "/cards/{cardId}/edit")
+	public String processUpdateForm(@Valid Card card, BindingResult result, @PathVariable("cardId") int cardId, ModelMap model) {
+		if (result.hasErrors()) {
+			model.put("card", card);
+			return VIEWS_CARDS_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+                        Card cardToUpdate=this.cardService.findCardById(cardId);
+			BeanUtils.copyProperties(card, cardToUpdate, "cardId","card_type");                                                                                  
+                    try {                    
+                        this.cardService.saveCard(cardToUpdate);                    
+                    } catch (DuplicateKeyException ex) {
+                        result.rejectValue("name", "duplicate", "already exists");
+                        return VIEWS_CARDS_CREATE_OR_UPDATE_FORM;
+                    }
+			return "redirect:/cards/{cardId}";
+		}
 	}
 }
--- src/main/java/org/springframework/samples/petclinic/model/CardService.java
+++ src/main/java/org/springframework/samples/petclinic/model/CardService.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +18,15 @@ public class CardService {
 	@Transactional
 	public Iterable<Card> cardFindAll() {
 		return cardRepository.findAll();
+	}
+	
+	@Transactional(readOnly = true)
+	public Card findCardById(int id) throws DataAccessException {
+		return cardRepository.findById(id);
+	}
+	
+	@Transactional
+	public void saveCard(Card cardToUpdate) throws DataAccessException {
+		cardRepository.saveCard(cardToUpdate);
 	}
 }
