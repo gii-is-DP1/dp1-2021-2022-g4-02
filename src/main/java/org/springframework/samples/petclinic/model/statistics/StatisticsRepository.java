63daab9dade935896d78ce6ad4d1b4e96e6c4e92
Miguel Ángel Rivas Rosado <migrivros@alum.us.es>
2021-11-02 19:56:46 +0100

    Añadiendo entidad,controlador, servicio y repositorio de Statistics

---
+++ src/main/java/org/springframework/samples/petclinic/model/statistics/StatisticsRepository.java
@@ -0,0 +1,8 @@
+package org.springframework.samples.petclinic.model.statistics;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface StatisticsRepository extends CrudRepository<Statistics, Integer>{
+
+}
