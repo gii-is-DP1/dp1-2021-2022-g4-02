a60758154acfd7c8317ea5dd8e694d42789daa37
RingoML <josemanuelmartinluque@outlook.com>
2021-11-05 12:04:03 +0100

    Comienzo de lobby

--- src/main/java/org/springframework/samples/petclinic/model/island/Island.java
+++ src/main/java/org/springframework/samples/petclinic/model/island/Island.java
@@ -4,6 +4,7 @@ package org.springframework.samples.petclinic.model.island;
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.model.card.Card;
@@ -16,6 +17,7 @@ import lombok.Setter;
 @Entity
 @Table(name = "isles")
 public class Island extends BaseEntity{
+	@NotEmpty
 	private Integer island;
 	@OneToOne(mappedBy = "cardId")
 	private Card card;
