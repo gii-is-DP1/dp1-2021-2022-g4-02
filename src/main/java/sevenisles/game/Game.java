c60c0857ec6cfd43ac5e4ff1ccc197baf4d7897f
Rafael Sanabriagit <rafsanesp@alum.us.es>
2021-11-19 14:27:58 +0100

    quitados import

--- src/main/java/sevenisles/game/Game.java
+++ src/main/java/sevenisles/game/Game.java
@@ -1,7 +1,6 @@
 package sevenisles.game;
 
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +8,6 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
