38af6a5ed521dc9e7b673ad821409b1f7cd973f9
Rafael Sanabriagit <rafsanesp@alum.us.es>
2021-11-26 12:01:18 +0100

    Empezada clase status y creado login manual

--- src/main/java/sevenisles/status/Status.java
+++ src/main/java/sevenisles/status/Status.java
@@ -1,12 +1,11 @@
-package sevenisles.player;
+package sevenisles.status;
 
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -14,34 +13,35 @@ import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import sevenisles.card.Card;
 import sevenisles.game.Game;
 import sevenisles.model.BaseEntity;
-import sevenisles.user.User;
+import sevenisles.player.Player;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "players")
-public class Player extends BaseEntity{
+@Table(name= "status")
+public class Status extends BaseEntity{
+	
+	@ManyToOne
+	private Game game;
 	
 	@OneToOne
-	@JoinColumn(name = "user_id", referencedColumnName = "id")
-	private User user;
+	private Player player;
+	
+	private Integer score;
 	
 	@OneToMany(cascade = CascadeType.ALL, targetEntity = Card.class, mappedBy="player")
 	private List<Card> cards;
 	
 	private Integer diceNumber;
 	
-	@ManyToOne(cascade = CascadeType.ALL, targetEntity = Game.class)
-	private Game game;
-
-
 	public Integer throwDice() {
 		this.diceNumber = ThreadLocalRandom.current().nextInt(1, 7);
 		return diceNumber;
 	}
+	
+	
 	
 }
