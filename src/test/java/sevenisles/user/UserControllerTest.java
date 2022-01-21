5a4b16ab497e8705b58e99f22972117dfa20e27b
Rafasana14 <rafasana9@gmail.com>
2022-01-21 19:09:10 +0100

    Añadido el logro de puntos

--- src/test/java/sevenisles/user/UserControllerTest.java
+++ src/test/java/sevenisles/user/UserControllerTest.java
@@ -20,7 +20,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ContextConfiguration;
